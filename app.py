1978d25b00b02fa17bf234649ff03e4b3a345588
tzrence <140904020+tzrence@users.noreply.github.com>
2026-07-28 10:30:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -382,7 +382,7 @@ elif page == "Performance & Insights":
         vehicle_counts.items(),
         columns=[
             "Vehicle",
-            "Days as a Top Performing Vehicle"
+            "Top 3 Appearances"
         ]
     )
 
